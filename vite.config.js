b8a4c38a0aafa370916d752c26fe914fbb24ac16
María Cecilia <lunama.cecilia@live.com>
2025-10-20 17:46:14 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          antd: ['antd']
+        }
+      }
+    }
+  },
+  base: './'
 })
