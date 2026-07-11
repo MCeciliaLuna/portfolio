b995a0ba3beeaba13e8a6f984970ee37c4b28026
María Cecilia <lunama.cecilia@live.com>
2026-07-11 20:43:21 -0300

    quito algunos proyectos

--- src/db/data.js
+++ src/db/data.js
@@ -25,6 +25,20 @@ const data = {
     },
     {
       id: uuidv4(),
+      "title": "Iglesia de Cristo Tucumán Web",
+      "description": "Landing page institucional desarrollada con ReactJs",
+      "imageUrl": "/images/projects/idc_optimized.webp",
+      "liveUrl": "https://iglesiadecristotuc.org/",
+    },
+     {
+      id: uuidv4(),
+      "title": "Plan Go APP",
+      "description": "Diseño de aplicación para agenda de eventos desarrollada en Figma",
+      "imageUrl": "/images/projects/PlanGO-app.webp",
+      "figmaUrl": "https://www.figma.com/proto/PIE5e4ga4ndZ4NrSnD9VOz/Pantallas-Finales?page-id=0%3A1&node-id=9-1100&p=f&viewport=261%2C173%2C0.28&t=VLgMJdHNW4zjYtsD-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=9%3A1115"
+    },
+    {
+      id: uuidv4(),
       "title": "Bless Inmobiliaria Web",
       "description": "Web empresarial desarrollada con ReactJs, backend y DB propia",
       "imageUrl": "/images/projects/blessinmobiliaria_optimized.webp",
@@ -35,20 +49,6 @@ const data = {
       "title": "Panel de Administración",
       "description": "Panel de administracion de Bless Inmobiliaria + backend propio",
       "imageUrl": "/images/projects/blessinmobiliaria-panel.webp"
-    },
-    {
-      id: uuidv4(),
-      "title": "Plan Go APP",
-      "description": "Diseño de aplicación para agenda de eventos desarrollada en Figma",
-      "imageUrl": "/images/projects/PlanGO-app.webp",
-      "figmaUrl": "https://www.figma.com/proto/PIE5e4ga4ndZ4NrSnD9VOz/Pantallas-Finales?page-id=0%3A1&node-id=9-1100&p=f&viewport=261%2C173%2C0.28&t=VLgMJdHNW4zjYtsD-1&scaling=scale-down&content-scaling=fixed&starting-point-node-id=9%3A1115"
-    },
-    {
-      id: uuidv4(),
-      "title": "Iglesia de Cristo Tucumán Web",
-      "description": "Landing page institucional desarrollada con ReactJs",
-      "imageUrl": "/images/projects/idc_optimized.webp",
-      "liveUrl": "https://iglesiadecristotuc.org/",
     },
     {
       id: uuidv4(),
@@ -64,34 +64,7 @@ const data = {
       "description": "API de frases de autor temáticas desarrollada con Express",
       "imageUrl": "/images/projects/phrases-api.webp",
       "repoUrl": "https://github.com/MCeciliaLuna/phrases-API"
-    },
-    {
-      id: uuidv4(),
-      "title": "To Do List",
-      "description": "WebApp checklist para gestionar tareas desarrollada con ReactJs",
-      "imageUrl": "/images/projects/todolist-web.webp",
-      "liveUrl": "https://todo-list-app-gl.netlify.app/",
-      "repoUrl": "https://github.com/MCeciliaLuna/to-do-list-GL",
-      "youtubeUrl": "https://youtu.be/CraOi6X3zLM?si=7KlX0O6uivIIo7oO"
-    },
-    {
-      id: uuidv4(),
-      "title": "La Quiaqueña Drugstore E-Commerce",
-      "description": "Plataforma de comercio electrónico completa con React y DB propia",
-      "imageUrl": "/images/projects/laquiaquena-web.webp",
-      "liveUrl": "https://laquiaquenadrugstores.netlify.app/",
-      "repoUrl": "https://github.com/MCeciliaLuna/laquiaquena-drugstores",
-      "youtubeUrl": "https://youtu.be/eZHqys0oeVI?si=-E7wKCQLom-QgO5O"
-    },
-    {
-      id: uuidv4(),
-      "title": "La Quiaqueña Herboristería E-Commerce",
-      "description": "Plataforma de comercio electrónico completa con React y DB propia",
-      "imageUrl": "/images/projects/laquiaquena-ii-web.webp",
-      "liveUrl": "https://laquiaquenaherboristeria.netlify.app/",
-      "repoUrl": "https://github.com/MCeciliaLuna/laquiaquena-herboristeria",
-      "youtubeUrl": "https://youtu.be/J1vSzS8Q2f0?si=smmlQNSsjfu64ybU"
-    },
+    }
   ],
   "skills": [
     {
